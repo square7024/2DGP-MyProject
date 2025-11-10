a4f261ea6b6cb65195556054021571ab27cfa9c3
MyMainPC <chajihun1171@gmail.com>
2025-11-10 21:54:31 +0900

    knight 화살표 이동 추가

--- test.py
+++ test.py
@@ -10,29 +10,43 @@ running = True
 
 def handle_events():
     global running
+    global x, dir
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
-open_canvas()
+
+open_canvas(1280, 1024)
 
 character = load_image('knight_spritesheet.png')
 
+x = 800 // 2
 frame = 0
+dir = 0
+
 while True:
     clear_canvas()
-    character.clip_draw(idle_sprite[frame][0], 2275, idle_sprite[frame][1] - idle_sprite[frame][0], 85, 400, 90, 200, 200)
+    character.clip_draw(idle_sprite[frame][0], 2275, idle_sprite[frame][1] - idle_sprite[frame][0], 85, x, 90, 200, 200)
     update_canvas()
-
     handle_events()
     if not running:
         break
-
     frame = (frame + 1) % 13
-    delay(0.1)
+    x += dir * 5
+    delay(0.01)
 
 close_canvas()
