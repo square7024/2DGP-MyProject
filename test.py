08be91cd3da1d44e151fda576133d8d6169d8663
MyMainPC <chajihun1171@gmail.com>
2025-11-10 22:25:24 +0900

    knight, forest class 추가

--- test.py
+++ test.py
@@ -6,47 +6,71 @@ idle_sprite = [
     (680, 748), (748, 814), (814, 882)
 ]
 
-running = True
+class Forest:
+    def __init__(self):
+        self.image = load_image('forest.png')
+
+    def draw(self):
+        self.image.draw(1280 / 2, 1024 / 2)
+
+    def update(self):
+        pass
+
+class Knight:
+    def __init__(self):
+        self.x, self.y = 400, 300
+        self.frame = 0
+        self.image = load_image('knight_spritesheet.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 13
+        self.x += 2
+
+    def draw(self):
+        self.image.clip_draw(idle_sprite[self.frame][0], 2275, idle_sprite[self.frame][1] - idle_sprite[self.frame][0], 85, self.x, self.y, 200, 200)
+
+
+def reset_world():
+    global running
+    global forest
+    global knight
+
+    running = True
+    forest = Forest()
+    knight = Knight()
+
+
+def update_world():
+    forest.update()
+    knight.update()
+
+
+def render_world():
+    clear_canvas()
+    forest.draw()
+    knight.draw()
+    update_canvas()
 
 def handle_events():
     global running
-    global x, dir
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                dir += 1
-            elif event.key == SDLK_LEFT:
-                dir -= 1
-            elif event.key == SDLK_ESCAPE:
+            if event.key == SDLK_ESCAPE:
                 running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dir -= 1
-            elif event.key == SDLK_LEFT:
-                dir += 1
 
 
 open_canvas(1280, 1024)
 
-character = load_image('knight_spritesheet.png')
+reset_world()
 
-x = 800 // 2
-frame = 0
-dir = 0
-
-while True:
-    clear_canvas()
-    character.clip_draw(idle_sprite[frame][0], 2275, idle_sprite[frame][1] - idle_sprite[frame][0], 85, x, 90, 200, 200)
-    update_canvas()
+while running:
     handle_events()
-    if not running:
-        break
-    frame = (frame + 1) % 13
-    x += dir * 5
+    update_world()
+    render_world()
     delay(0.01)
 
 close_canvas()
