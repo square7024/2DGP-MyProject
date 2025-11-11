34098c7a1ccc9bbdd4349b9a0241c6a3e8229001
MyMainPC <chajihun1171@gmail.com>
2025-11-11 09:35:16 +0900

    프레임 메시지 off

--- game_framework.py
+++ game_framework.py
@@ -55,10 +55,11 @@ def run(start_mode):
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
+
         frame_time = time.time() - current_time
         frame_rate = 1.0 / frame_time
         current_time += frame_time
-        print(f"Frame Time: {frame_time}, Frame Rate: {frame_rate}")
+        # print(f"Frame Time: {frame_time}, Frame Rate: {frame_rate}")
 
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
