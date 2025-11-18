e03b8fd935657058568631f13b9b14b57d0b1b1c
MyMainPC <chajihun1171@gmail.com>
2025-11-18 09:25:00 +0900

    변수이름 변경

--- state_machine.py
+++ state_machine.py
@@ -1,27 +1,26 @@
 from event_to_string import event_to_string
 
 class StateMachine:
-    def __init__(self, start_state, rules):
+    def __init__(self, start_state, state_transitions):
         self.cur_state = start_state
-        self.rules = rules
+        self.state_transitions = state_transitions
         self.cur_state.enter(("START", None))   # 시작할때 가상의 이벤트 전달
 
     def update(self):
         self.cur_state.do()
 
-    def draw(self):
-        self.cur_state.draw()
-
-    def handle_state_event(self, state_event):
-        # state_event가 어떤 이벤트인지 체크
-        for check_event in self.rules[self.cur_state].keys():
-            if check_event(state_event):
-                self.next_state = self.rules[self.cur_state][check_event]
-                self.cur_state.exit(state_event)
-                self.next_state.enter(state_event)
-                print(f'State Transition: {self.cur_state.__class__.__name__} -> {self.next_state.__class__.__name__}')
+    def handle_state_event(self, event):
+        for check_event in self.state_transitions[self.cur_state].keys():
+            if check_event(event):
+                self.cur_state.exit(event)
+                self.next_state = self.state_transitions[self.cur_state][check_event]
+                self.next_state.enter(event)
+                print(f'{self.cur_state.__class__.__name__} ---- {event_to_string(event)} ----> {self.next_state.__class__.__name__}')
                 self.cur_state = self.next_state
                 return
 
         # 이벤트에대한 처리가 안됐다.. 따라서 문제가 발생.
-        print(f'처리되지 않은 이벤트 {event_to_string(state_event)} 가 있습니다.')
+        print(f'처리되지 않은 이벤트 {event_to_string(event)} 가 있습니다.')
+
+    def draw(self):
+        self.cur_state.draw()
